f6e5d29b4d5636f4f7e077ef8fa059bf1456676d
Daniel Bowen <djbowen95@gmail.com>
2022-01-04 17:26:58 +0000

    save password characters in several arrays, sorted by type

---
+++ README.md
@@ -0,0 +1,10 @@
+Created the first five lines of code - an empty array for all the available password characters, and 4 arrays containing: uppercase, lowercase, numbers and special characters.
+
+The idea is that I will get the prompt box to ask:
+"Which kind of characters would you like to include?"
+"Numbers, Uppercase Letters, Lowercase Letters, Special Characters"
+
+Problems from first session working:
+    - Not sure how to change HTML/CSS page display at all
+    - not sure how to do if without else (maybe just 'if')
+    
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+/* Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -11,4 +11,19 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); */
+
+console.log("hello")
+
+let passwordCharacters = [];
+const passwordCapitals = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "L", "M", "N", "O", "P", "Q", "R", "S","T", "U", "V", "W", "X", "Y", "Z"];
+const passwordLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const passwordNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// Doesn't contain the backslash ('\') character
+const passwordSpecialCharacters = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+let wantCapitals = ""
+wantCapitals ? console.log("hi") : console.log("bye"); //add to passwordCharacters
+
+let wantLowercase = ""
+wantLowercase ? console.log("hi") : console.log("bye"); //add to passwordCharacters
