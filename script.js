0314eca393fe8de7188f09c74b807c5a876473c4
Daniel Bowen <djbowen95@gmail.com>
2022-01-09 21:50:35 +0000

    function to set character length based on a prompt

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Create / store the user's input for character selection. 
-// If all 'false' then must give 'error' message.
-let addCaps = true;
-let addLower = true;
-let addDigits = true;
-let addSpecials = true;
+// Alert/confirmation boxes - check if they want each type of character.
+// Could probably do this in one function, and save the results as an array. 
+function checkCapitals() {
+  if (confirm("Do you want to include capital letters?") == true) {
+    return true;
+  } else {return false}
+};
 
-// Create / store the users input for chosen length.
-// Must be more than 8 and less than 128. 
-let chosenLength = 12;
+function checkLower() {
+  if (confirm("Do you want to include lowercase letters?") == true) {
+    return true;
+  } else {return false}
+}
+
+function checkDigits() {
+  if (confirm("Do you want to include numbers?") == true) {
+    return true;
+  } else {return false}
+}
+
+function checkSpecials() {
+  if (confirm("Do you want to include special characters?") == true) {
+    return true;
+  } else {return false}
+}
+
+// Function to check at least one type of character is chosen. 
+function checkSomethingChosen() {
+}
 
 //Add all desired characters into a single string value.
 function selectCharacters() {
+  let addCaps = checkCapitals();
   let chosenCharacters = "";
   if (addCaps === true) {
     const passwordCapitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     chosenCharacters = chosenCharacters.concat(passwordCapitals);
   }
+  let addLower = checkLower();
   if (addLower === true) {
     const passwordLowercase = "abcdefghijklmnopqrstuvwxyz";
     chosenCharacters = chosenCharacters.concat(passwordLowercase);
   }
+  let addDigits = checkDigits();
   if (addDigits === true) {
     const passwordDigits = "0123456789";
     chosenCharacters = chosenCharacters.concat(passwordDigits);
   }
+  let addSpecials = checkSpecials();
   if (addSpecials === true) {
     const passwordSpecialCharacters = "!\"#$%&'()*,-./:;<=>?@[]^_`{}|~";
     chosenCharacters = chosenCharacters.concat(passwordSpecialCharacters);
@@ -34,10 +57,24 @@ function selectCharacters() {
   return chosenCharacters;
 }
 
+// Create / store the users input for chosen length.
+// Must be more than 8 and less than 128. 
+function chooseLength() {
+  let userInput = prompt("How many characters would you like? Please pick a number between 8 and 128.", "8");
+  if (userInput >= 8 && userInput <= 128) {
+    return userInput;
+  } else {
+    alert("Invalid Input. We will give you a default password of length 8.");
+    return 8;
+  }
+}
+
 // Randomize a password of chosenLength from character pool of selectCharacters;
 function generatePassword() {
   let chosenCharacters = selectCharacters();
+  let chosenLength = chooseLength();
   let totalCharOptions = chosenCharacters.length;
+  
   let password = "";
   for (let passLength = 0; passLength < chosenLength; passLength++) {
     let randNum = Math.random() * totalCharOptions;
@@ -58,21 +95,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-/*
-Function that when clicked ->
-  Runs the function writePassword
-
-
-/*
-let passwordCharacters = [];
-const passwordNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-// Doesn't contain the backslash ('\') character
-const passwordSpecialCharacters = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
-    const passwordCapitals = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T", "U", "V", "W", "X", "Y", "Z"];
-
-let wantCapitals = ""
-wantCapitals ? console.log("hi") : console.log("bye"); //add to passwordCharacters
-
-let wantLowercase = ""
-wantLowercase ? console.log("hi") : console.log("bye"); //add to passwordCharacters */
