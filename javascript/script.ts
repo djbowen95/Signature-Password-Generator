33f10c335c0e668dc0541e0c5b3a24071ce94c8b
Daniel Bowen <djbowen95@gmail.com>
2022-07-16 14:45:14 +0100

    Outline all steps from old script in script.js

---
+++ javascript/script.ts
@@ -0,0 +1,7 @@
+// Fetch elements from the DOM.
+// Prompt user - characters
+// Prompt user - length (can I limit?)
+// Add characters (arrays)
+// Select random array
+// Create string
+// Output string in relevant HTML spot.
